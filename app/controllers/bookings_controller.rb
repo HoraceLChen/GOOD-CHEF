3e3c18119c31edc6789d47eb915fea48a71948e2
Vincent Tee <vincent.teeym@gmail.com>
2023-08-15 17:17:31 +1000

    Booking Params

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,13 @@
 class BookingsController < ApplicationController
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save?
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:user_id, :chef_offer_id, :date, :status)
+  end
 end
