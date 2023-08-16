d43d96110db3e782000b5d9d615a9224a4071bcc
Horace Chen <horacechen2@gmail.com>
2023-08-16 16:31:14 +1000

    index cards test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_16_042804) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_16_043650) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,7 +21,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_16_042804) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "guests"
+    t.bigint "user_id", null: false
     t.index ["offer_id"], name: "index_bookings_on_offer_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "offers", force: :cascade do |t|
@@ -51,5 +53,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_16_042804) do
   end
 
   add_foreign_key "bookings", "offers"
+  add_foreign_key "bookings", "users"
   add_foreign_key "offers", "users"
 end
