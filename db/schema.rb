6142638a5d578227d0dd9a9e64f9220a950ae262
exec zsh <kaishu0520@gmail.com>
2023-08-16 16:31:40 +0900

    edited html and css in booking/index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_15_075900) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_16_055903) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,10 +18,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_15_075900) do
     t.date "date"
     t.string "status"
     t.bigint "offer_id", null: false
-    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "guests"
+    t.bigint "user_id", null: false
     t.index ["offer_id"], name: "index_bookings_on_offer_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
@@ -31,10 +31,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_15_075900) do
     t.integer "price_pp"
     t.text "description"
     t.string "title"
-    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_offers_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -54,5 +52,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_15_075900) do
 
   add_foreign_key "bookings", "offers"
   add_foreign_key "bookings", "users"
-  add_foreign_key "offers", "users"
 end
