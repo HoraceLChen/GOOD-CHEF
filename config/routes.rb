1515bf775232617b5dcc636c1d5de8d8d4c626a1
exec zsh <kaishu0520@gmail.com>
2023-08-17 17:37:22 +0900

    Added search box, edit and update an offer, edited show page　a bit

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   # root to: "/"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :offers, only: [:index, :show, :new, :create] do
+  resources :offers, only: [:index, :show, :new, :create, :edit, :update] do
     resources :bookings, only: [:create]
   end
   resources :bookings, only: [:index, :update]
   # Defines the root path route ("/")
   # root "articles#index"
   root "offers#index"
+
+  Rails.application.routes.draw do
+    # root to: "pages#home"
+    resources :offers, only: :index
+  end
 end
