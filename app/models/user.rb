103c469abe1d33d2cd55533ab37ddcae26780885
Vincent Tee <vincent.teeym@gmail.com>
2023-08-18 16:37:17 +1000

    Reviews

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,9 @@ class User < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :offers, dependent: :destroy
   has_many :bookings_as_chef, through: :offers, source: :bookings
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_17_062343) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_18_035139) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,7 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_17_062343) do
 
   create_table "bookings", force: :cascade do |t|
     t.date "date"
-    t.string "status"
+    t.integer "status", default: 0
     t.bigint "offer_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -69,6 +69,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_17_062343) do
     t.index ["user_id"], name: "index_offers_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "description"
+    t.bigint "offer_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["offer_id"], name: "index_reviews_on_offer_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -79,7 +90,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_17_062343) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
-    t.string "role"
+    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -89,4 +100,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_17_062343) do
   add_foreign_key "bookings", "offers"
   add_foreign_key "bookings", "users"
   add_foreign_key "offers", "users"
+  add_foreign_key "reviews", "offers"
+  add_foreign_key "reviews", "users"
 end
